79b1007076366c277bf02c0764d2144c520d7d67
Hirokazu Fukami <fkm@mokelab.com>
2020-06-25 02:21:51 +0900

    Add adapter library / Fix mimegroup feature

--- features/mimegroup/src/main/java/com/mokelab/demo/android11/mimegroup/MimeGroupFragment.kt
+++ features/mimegroup/src/main/java/com/mokelab/demo/android11/mimegroup/MimeGroupFragment.kt
@@ -5,11 +5,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.google.android.material.snackbar.Snackbar
+import com.mokelab.demo.android11.adapter.SimpleListAdapter
+import com.mokelab.demo.android11.adapter.SimpleStringListAdapter
 import com.mokelab.demo.android11.mimegroup.databinding.MimeGroupFragmentBinding
 
 class MimeGroupFragment : Fragment(R.layout.mime_group_fragment) {
-    var binding: MimeGroupFragmentBinding? = null
+    private var binding: MimeGroupFragmentBinding? = null
+    private var adapter: SimpleStringListAdapter? = null
+
+    private val vm: MimeGroupViewModel by viewModels()
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -18,27 +26,40 @@ class MimeGroupFragment : Fragment(R.layout.mime_group_fragment) {
 
         binding.addButton.setOnClickListener {
             val type = binding.addTypeEdit.text.toString()
-            addMimeType(type)
+            vm.addType(type)
+            binding.addTypeEdit.setText(R.string.empty)
+        }
+
+        binding.submitButton.setOnClickListener {
+            vm.submitTypes()
+            Snackbar.make(requireView(), R.string.done_submit, Snackbar.LENGTH_SHORT).show()
         }
 
         binding.viewButton.setOnClickListener {
             val type = binding.viewTypeEdit.text.toString()
             viewWithType(type)
         }
+
+        this.adapter =
+            SimpleStringListAdapter(object : SimpleListAdapter.ItemClickListener<String> {
+                override fun onItemClicked(item: String, position: Int) {
+                    vm.removeType(item)
+                }
+            })
+        binding.recycler.adapter = this.adapter
+
+        this.vm.mimeTypes.observe(viewLifecycleOwner, {
+            this.adapter?.submitList(it.toList().sorted())
+        })
+
+        if (savedInstanceState == null) {
+            vm.loadTypes()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         this.binding = null
-    }
-
-    private fun addMimeType(type: String) {
-        val pm = requireContext().packageManager
-        val types = setOf(type)
-        pm.setMimeGroup(MIME_GROUP, types)
-
-        this.binding?.addTypeEdit?.setText(R.string.empty)
-        Snackbar.make(requireView(), R.string.done_add, Snackbar.LENGTH_SHORT).show()
     }
 
     private fun viewWithType(type: String) {
@@ -52,9 +73,4 @@ class MimeGroupFragment : Fragment(R.layout.mime_group_fragment) {
                 .show()
         }
     }
-
-    companion object {
-        private const val MIME_GROUP = "com.mokelab:demoFeature"
-    }
-
 }
